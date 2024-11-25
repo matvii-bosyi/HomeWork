aeb3c60ad799ff6954a44abe5eb24602d8334b77
matvii <bosyimatvii@gmail.com>
2024-11-25 22:29:31 +0200

    valera-work

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ document.getElementById('burger-menu').addEventListener('click', function () {
 	const nav = document.querySelector('ul')
 	nav.classList.toggle('active')
 })
+
+// valera-work
+// 1234
+// 1234
